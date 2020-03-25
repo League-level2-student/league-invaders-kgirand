b5148c9dea054d45308b73db861e6c860665187d
Kazuki <kgirand@yahoo.com>
2020-03-25 13:29:14 -0700

    tsey
    esrtter

--- src/LeagueInvaders.java
+++ src/LeagueInvaders.java
@@ -1,4 +1,19 @@
+import javax.swing.JFrame;
 
 public class LeagueInvaders {
-
+JFrame frame;
+public static final int WIDTH;
+public static final int HEIGHT;
+LeagueInvaders(){
+	frame = new JFrame();
+	
+}
+void setup(){
+	WIDTH = 500;
+	HEIGHT = 800;
+	frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+}
+	public static void main(String[] args) {
+	
+}
 }
